6be2eb9e455f282c63c3d86aef13292506111868
Anuradha Ashavatha Rao <anuradha.ashavatharao@citrix.com>
2020-08-26 14:27:11 -0400

    latest update

--- prod.tf
+++ prod.tf
@@ -10,5 +10,56 @@ resource "aws_s3_bucket" "prod_tf_course" {
 }
 
 resource "aws_default_vpc" "default" {
-    
+
 }
+
+resource "aws_security_group" "prod_web"{
+    name        = "prod_web"
+    description = "Allow std HTTP and HTTP ports inbound and everything outbound"
+
+    ingress {
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port   = 443
+        to_port     = 443
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port  = 0
+        to_port    = 0
+        protocol   = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        "Terraform" : "true"
+    }
+}
+
+resource "aws_instance" "prod_web" {
+    ami                    = "ami-0743f105d738afe6a"
+    instance_type          = "t2.nano"
+    vpc_security_group_ids = [
+            aws_security_group.prod_web.id
+    ]
+    tags = {
+            "Terraform" : "true"
+    }
+}
+
+resource "aws_eip" "rod_web" {
+    instance = aws_instance.prod_web.id
+    
+
+    tags = {
+            "Terraform" : "true"
+    }
+}
+
